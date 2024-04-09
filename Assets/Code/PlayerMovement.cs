c114fac33f9c45ca03af50533f5a9da62e200f09
Blaze-Darkwood <darkwoodblaze@gmail.com>
2024-04-09 11:50:15 +0200

    Player movement + Camera
    Added smooth player movement and a 3rd person working camera. (Check by others)

---
+++ Assets/Code/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Camera _mainCam;
+    private Rigidbody _rigidBody;
+
+    [SerializeField] private float _MovementSpeed;  // 20000
+    [SerializeField] private int _Sensitivity;      // 5
+
+    void Start()
+    {
+        _mainCam = Camera.main;
+        _rigidBody = GetComponent<Rigidbody>();
+
+        //locks the cursor to the middle of the screen
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (Cursor.lockState == CursorLockMode.Locked)
+            LookAround();
+
+        //Makes the player move with [W,A,S,D]
+        _rigidBody.AddForce(transform.right * (Input.GetAxis("Horizontal") * _MovementSpeed * Time.deltaTime));
+        _rigidBody.AddForce(transform.forward * (Input.GetAxis("Vertical") * _MovementSpeed * Time.deltaTime));
+    }
+
+    private void LookAround()
+    {
+        //Rotates the Camera around the player model on the Y Axis
+        _mainCam.transform.localRotation = Quaternion.Euler(30, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * _Sensitivity, 0);
+    }
+}
