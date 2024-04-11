7dee3cfde5b7bd1e8b21d8753f3ef284a4c42e83
Gerben Bol <gerben.bol249@gmail.com>
2024-04-11 11:40:01 +0200

    control timer update

--- Assets/Code/UI/ControlScreen.cs
+++ Assets/Code/UI/ControlScreen.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ControlScreen : MonoBehaviour
 {
-    private readonly float maxTimer = 3f;
+    private readonly float maxTimer = 4f;
     private float timer = .0f;
 
     private void Update()
