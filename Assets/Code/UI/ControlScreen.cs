2981742b2ae74cdd67ded7cf9b912e6ef683daf3
Gerben Bol <gerben.bol249@gmail.com>
2024-04-11 11:30:07 +0200

    control screen

---
+++ Assets/Code/UI/ControlScreen.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlScreen : MonoBehaviour
+{
+    private readonly float maxTimer = 3f;
+    private float timer = .0f;
+
+    private void Update()
+    {
+        if (timer < maxTimer)
+            timer += Time.unscaledDeltaTime;
+        else
+        {
+            Time.timeScale = 1;
+            gameObject.SetActive(false);
+        }
+    }
+}
